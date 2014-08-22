3c4d8b1eb60e87856aa7fea1d6c6dd2e9e2b863c
muktaj <mukta.joglekar@gmail.com>
2014-08-22 12:30:58 -0400

    default route changed

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  console.log("Default route");
-  res.render('index', { title: 'Express' });
-});
-
-router.get('/welcome.html', function(req, res) {
         console.log("request for : "+req);
         fs.readFile("./welcome.html", function(err, html) {
         console.log("In readFile");
